d3283f34d2dd28b7baee02066bca797918bdaf42
SelineCCC <seline0504.chen@gmail.com>
2022-05-25 18:30:27 -0700

    submitting

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+admin.site.register(Category, CategoryAdmin)
